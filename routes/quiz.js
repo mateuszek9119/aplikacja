022bfdfcc377ee34bf5f950d9723c708d3640744
mateusz <mateusz@gmail.com>
2024-11-04 17:41:06 +0100

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('quiz', { title: 'Quiz' });
+
+  const showForm = req.session.vote
+
+  let sum = 0
+
+  Quiz.find()
+    .then((data) => {
+      data.forEach((item) => {
+        sum = sum + item.vote
+      })
+      res.render('quiz', { title: 'Quiz', data, showForm, sum });
+    })
 });
+
+router.post('/', (req, res) => {
+
+  const id = req.body.answer
+
+  Quiz.findById({ _id: id })
+    .then((data) => {
+      data.vote += 1
+      data.save()
+        .then(() => {
+          req.session.vote = 1
+          res.redirect('/quiz')
+        })
+    })
+})
 
 module.exports = router;
