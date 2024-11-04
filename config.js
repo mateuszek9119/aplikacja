339d3df18ab0adb975fb7759c25aec088816a7e3
mateusz <mateusz@gmail.com>
2024-11-04 18:07:35 +0100

    config changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "",
+  db: "mongodb+srv://admin:wYoXTHkMV60VnRZY@cluster0.15mpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   keySession: ["KLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 }
